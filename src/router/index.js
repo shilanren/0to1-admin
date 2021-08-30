e6969022423c86af76a6eb1af4335750a1070ece
shiguanglong <guanglong.shi@iunogame.com>
2021-08-30 14:13:00 +0800

    feat: 增加一个demo路由，增加一个demo页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+// 布局
+import Layout from '../views/layout/Layout'
+// 首页
+import Dashboard from '../views/dashboard/index'
+// 测试页面
+import Test from '../views/text/index'
 
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: '首页',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      component: Dashboard
+    }]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/text',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        component: Test,
+        name: '测试'
+      }
+    ]
   }
 ]
 
